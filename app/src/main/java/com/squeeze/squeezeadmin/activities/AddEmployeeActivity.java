c45461ba34ae895c9de1e24c1a53d20bca28574a
tarzan212 <activation212@gmail.com>
2018-07-02 17:41:37 -0300

    Designed most of the first step

---
+++ app/src/main/java/com/squeeze/squeezeadmin/activities/AddEmployeeActivity.java
@@ -0,0 +1,15 @@
+package com.squeeze.squeezeadmin.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.squeeze.squeezeadmin.R;
+
+public class AddEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_employee);
+    }
+}
---
+++ app/src/main/java/com/squeeze/squeezeadmin/activities/TrainNetworkActivity.java
@@ -0,0 +1,15 @@
+package com.squeeze.squeezeadmin.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.squeeze.squeezeadmin.R;
+
+public class TrainNetworkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_train_network);
+    }
+}
